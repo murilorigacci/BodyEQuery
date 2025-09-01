c5e71f79b8d51dec2a2ebb83ee539a88d33cd08a
Murilo <murilo.r.rocha@aluno.senai.br>
2025-09-01 16:25:42 -0300

    Modifica server.js

--- server.js
+++ server.js
@@ -22,34 +22,70 @@ app.get("/", (req, res) => {
 // Aqui vão todas suas Rotas
 
 
-// Iniciar servidor escutando na porta definida
-app.listen(serverPort, () => {
-    console.log(`🚀 Servidor rodando em http://localhost:${serverPort} 🚀`);
-});
 
 // Query Parameters no Node.js - API de Hogwarts
 app.get('/bruxos', (req, res) => {
-    const { casa, ano, especialidade, nome } = req.query;
-    let resultado = bruxos;
+  const { casa, ano, especialidade, nome, varinha } = req.query;
+  let resultado = bruxos;
   
-    if (casa) {
-      resultado = resultado.filter(b => b.casa.toLowerCase() === casa.toLowerCase());
-    }
+  if (casa) {
+    resultado = resultado.filter(b => b.casa.toLowerCase() === casa.toLowerCase());
+  }
   
-    if (ano) {
-      resultado = resultado.filter(b => b.ano == ano);
-    }
+  if (ano) {
+    resultado = resultado.filter(b => b.ano == ano);
+  }
   
-    if (especialidade) {
-      resultado = resultado.filter(b => b.especialidade.toLowerCase().includes(especialidade.toLowerCase()));
-    }
+  if (especialidade) {
+    resultado = resultado.filter(b => b.especialidade.toLowerCase().includes(especialidade.toLowerCase()));
+  }
   
-    if (nome) {
-      resultado = resultado.filter(b => b.nome.toLowerCase().includes(nome.toLowerCase()));
-    }
+  if (nome) {
+    resultado = resultado.filter(b => b.nome.toLowerCase().includes(nome.toLowerCase()));
+  }
+
+  if (varinha) {
+    resultado = resultado.filter(v => v.varinha.toLowerCase().includes(varinha.toLowerCase()));
+  }
   
-    res.status(200).json({
-      total: resultado.length,
-      data: resultado
-    });
+  res.status(200).json({
+    total: resultado.length,
+    data: resultado
+  });
+});
+
+app.post("/bruxos", (req, res) => {
+  const { nome, casa, ano, varinha, mascote, patrono, especialidade, vivo } = req.body;
+
+  if (!nome || !casa) {
+    return res.status(400).json({
+      success : false, 
+      message: "Nome e casa são obrigatórios para um bruxo!"
+    })
+  }
+  const novoBruxo = {
+    id: bruxos.length + 1,
+    nome,
+    casa,
+    ano: parseInt(ano),
+    varinha,
+    mascote,
+    patrono,
+    especialidade: especialidade || "Ainda não atribuído!",
+    vivo: vivo
+  }
+  
+  bruxos.push(novoBruxo);
+  
+  res.status(201).json({
+    success: true,
+    message: "novo bruxo adicionado a Hogwarts!",
+    data: novoBruxo,
+  });
+});
+
+
+// Iniciar servidor escutando na porta definida
+app.listen(serverPort, () => {
+    console.log(`🚀 Servidor rodando em http://localhost:${serverPort} 🚀`);
 });
