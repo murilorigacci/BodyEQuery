5134da3a6e43b76d90758a48878a97bf04ff12ea
Murilo <murilo.r.rocha@aluno.senai.br>
2025-09-04 23:16:52 -0300

    Modivica o arquivo server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import dados from "./src/data/dados.js";
-const  {bruxos}  = dados
+const  {bruxos, varinhas, pocoes, animais}  = dados
 
 // Criar aplicação com Express e configurar para aceitar JSON
 const app = express();
@@ -44,15 +44,70 @@ app.get('/bruxos', (req, res) => {
     resultado = resultado.filter(b => b.nome.toLowerCase().includes(nome.toLowerCase()));
   }
 
-  if (varinha) {
-    resultado = resultado.filter(v => v.varinha.toLowerCase().includes(varinha.toLowerCase()));
-  }
-  
   res.status(200).json({
     total: resultado.length,
     data: resultado
   });
 });
+
+app.get('/varinhas', (req,res) => {
+  const { material, nucleo, comprimento } =req.query;
+  let resultado = varinhas;
+
+  if (material) {
+    resultado = resultado.filter(v => v.material.toLowerCase().includes(material.toLocaleLowerCase()))
+  }
+
+  if (nucleo) {
+    resultado = resultado.filter(v => v.nucleo.toLowerCase().includes(nucleo.toLocaleLowerCase()))
+  }
+
+  if (comprimento) {
+    resultado = resultado.filter(v => v.comprimento.toLowerCase().includes(comprimento.toLocaleLowerCase()))
+  }
+
+  res.status(200).json({
+    total: resultado.length,
+    data: resultado
+  });
+});
+
+  app.get('/pocoes', (req,res) => {
+    const { nome, efeito } =req.query;
+    let resultado = pocoes;
+
+    if (nome) {
+      resultado = resultado.filter(v => v.nome.toLowerCase().includes(nome.toLocaleLowerCase()))
+    }
+
+    if (efeito) {
+      resultado = resultado.filter(v => v.efeito.toLowerCase().includes(efeito.toLocaleLowerCase()))
+    }
+
+    res.status(200).json({
+    total: resultado.length,
+    data: resultado
+    });
+  });
+
+  app.get('/animais', (req,res) => {
+    const { tipo, nome } =req.query;
+    let resultado = animais;
+
+    if (tipo) {
+      resultado = resultado.filter(v => v.tipo.toLowerCase().includes(tipo.toLocaleLowerCase()))
+    }
+
+    if (efeito) {
+      resultado = resultado.filter(v => v.efeito.toLowerCase().includes(efeito.toLocaleLowerCase()))
+    }
+
+    res.status(200).json({
+    total: resultado.length,
+    data: resultado
+    });
+  });
+  
 
 app.post("/bruxos", (req, res) => {
   const { nome, casa, ano, varinha, mascote, patrono, especialidade, vivo } = req.body;
@@ -83,6 +138,33 @@ app.post("/bruxos", (req, res) => {
     data: novoBruxo,
   });
 });
+
+app.post("varinhas", (req, res) => {
+  const { material, nucleo, comprimento } =req.body;
+
+  if (!material || !nucleo || !comprimento) {
+    return res.status(400).json({
+      success: false,
+      message: "Material, núcleo e comprimento são obrigat´rios para uma varinha!",
+    })
+  }
+
+  const novaVarinha = {
+    id: varinhas.length + 1,
+    material,
+    nucleo,
+    comprimento
+  }
+
+  variinhas.push(novaVarinha)
+
+  res.status(201).jason({
+    sucess: true,
+    message: "Uma nova varinha foi adicionada a Hogwarts!",
+    data: novaVarinha,
+    })
+  });
+
 
 
 // Iniciar servidor escutando na porta definida
